58ae7ed925a3e073974764061aaac2535f6f5268
Leonel <leonelebi@gmail.com>
2020-07-26 04:02:53 -0300

    RNN added

--- readme.txt
+++ readme.txt
@@ -2,6 +2,8 @@ STOCK MARKET PREDICTION WITH HADES
 
 Web app based on machine learning algorithms to predict the stock market. 
 
-To create the app we use Streamlit. Then we import yfinance library and with these data prediction is possible using machine learning models. 
+We use yfinance data to predict the price of a company using LSTM deep learning architecture. 
+Its just a game, and dont must be used as a trade advice. 
+Future considerations: fundamental analysis.
 
 # own production ##
--- stock_data.py
+++ stock_data.py
@@ -11,13 +11,6 @@ st.write("""
 This app predicts the Stock´s Price of any Company!
 """)
 
-days = []
-months = []
-years = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
-for i in range(1,32):
-    days.append(i)
-    if i<13:
-        months.append(i)
 
 
 st.sidebar.header('User Input Parameters')
@@ -26,52 +19,152 @@ add_selectbox = st.sidebar.selectbox(
     "What company or cryptocurrency are you interested in?",
     ("AAPL", "GOOGL", "MELI","GGAL", "TSLA", "BTC-USD", "ETH-USD", "ETH-BTC", "USDARS=X")
 )
-day_0 = st.sidebar.selectbox(
-    "Initial day",
-    days
-)
 
-month_0 = st.sidebar.selectbox(
-    "Initial month",
-    months
-)
-year_0 = st.sidebar.selectbox(
-    "Initial year",
-    years
-)
 
-start = f"{year_0}-{month_0}-{day_0}"
+
 today = date.today()
 input_data = {"company" : add_selectbox,
-                "day_0" : day_0,
-                "month_0" : month_0,
-                "year_0" : year_0} 
+                } 
 features = pd.DataFrame(input_data, index=[0])
 
-tickerData = yf.Ticker(input_data["company"])
-tickerDf = tickerData.history(period='1d', start=start, end=today)
 
+# Neural Network model to predict the stock market 
+
+# Libraries
+import pandas_datareader as web
+import numpy as np
+import math 
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
+from datetime import datetime
+from datetime import timedelta, date
+
+
+# Input data
+stock = input_data["company"]
+start = "2000-01-01"
+end = datetime.today()
+tomorrow = pd.to_datetime(end) + pd.DateOffset(days=1)
+previous_days = 1
+datetime.today()
+
+# Import the stock dataset
+df = web.DataReader(stock, data_source = "yahoo", start = start, end = end)
+
+# Dataset only with Close column
+data = df.filter(["Close"]) # New dataset with only "Close" column
+dataset = data.values # transform the data into a numpy array
+
+#Graphs
 st.write(f"""
 ## {input_data["company"]} Closing Price
 """)
-st.line_chart(tickerDf.Close)
+fig = plt.figure(figsize = (16,8))
+plt.title(f"${stock} Close Price History", size = 30)
+plt.plot(df["Close"])
+plt.xlabel("Date", fontsize = 18)
+plt.ylabel("Close Price", fontsize = 18)
+plt.legend([f"${stock} Close Price"], fontsize=20)
+plt.grid()
+plt.grid(linestyle='-', linewidth='0.5', color='red')
+#plt.show()
+st.pyplot()
+
 st.write(f"""
 ## {input_data["company"]} Volume Price
 """)
-st.line_chart(tickerDf.Volume)
+fig = plt.figure(figsize = (16,8))
+plt.title(f"${stock} Volume", size = 30)
+plt.plot(df["Volume"])
+plt.xlabel("Date", fontsize = 18)
+plt.ylabel("Volume", fontsize = 18)
+plt.legend([f"${stock} Volume"], fontsize=20)
+plt.grid()
+plt.grid(linestyle='-', linewidth='0.5', color='red')
+#plt.show()
+st.pyplot()
 
+# Decide the amount of instances for train and test
+train_examples = math.ceil(len(dataset)*0.8) # amount of train instances
+test_explames =  len(dataset) - train_examples# amount of test instances
+
+# Generate the train and test dataset
+train_data = dataset[0:train_examples, :]
+test_data = dataset[train_examples-previous_days: , : ]
+
+# Dataset scaled (values between 0-1)
+scaler = MinMaxScaler(feature_range = (0,1))
+train_data_scaled = scaler.fit_transform(train_data)
+test_data_scaled = scaler.fit_transform(test_data)
+
+    # Split the train set into inputs (x) and outputs (y)
+x_train = []
+y_train = []
+for i in range(previous_days, len(train_data_scaled)):
+    x_train.append(train_data_scaled[i-previous_days:i, 0])
+    y_train.append(train_data_scaled[i,0])
+
+    # COnvert into numpy arrays
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+        # Split the test set into inputs(x) and outputs(y)
+x_test = []
+y_test = scaler.fit_transform(dataset[train_examples: , : ])
+for i in range(previous_days, len(test_data_scaled)):
+    x_test.append(test_data_scaled[i-previous_days:i, 0])
+
+    # COnvert into numpy arrays
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+        # Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    # Deep Learning model
+model = Sequential() #creation of the model
+model.add(LSTM(50, return_sequences=True, input_shape = (x_train.shape[1], 1))) # input layer
+model.add(LSTM(50, return_sequences=False)) # hidden layer
+model.add(Dense(25)) # hidden layer
+model.add(Dense(1)) # output layer
+
+# Compile the neural network
+model.compile(optimizer = "adam", loss = "mean_squared_error")
+
+# Train the model
+model.fit(x_train, y_train, batch_size = 1, epochs = 2,verbose=0)
+
+# TOMORROW PRICE: 2019-12-18
+df_future = df.filter(["Close"])
+last_60_days = df_future[-previous_days:].values
+last_60_days_scaled = scaler.transform(last_60_days)
+x_future = []
+x_future.append(last_60_days_scaled)
+x_future = np.array(x_future)
+x_future = np.reshape(x_future, (x_future.shape[0], x_future.shape[1], 1))
+price_future = model.predict(x_future)
+price_future = scaler.inverse_transform(price_future)
+print(F"${stock} Close Price - Tomorrow {tomorrow} - Model: $ {((price_future[0][0]))}")
+price = price_future[0][0]
+#print(df)
 
 st.write(f"""
-## {input_data["company"]} Prediction Closing Price
-""")
+## {input_data["company"]} Price Predicted for Tomorrow
+ {price}""")
 
 
-chart_data = pd.DataFrame(
-    tickerDf.Close
-    )
-st.line_chart(chart_data)
 
 
-arr = yf.download("AAPL", start="2020-04-25", end="2020-04-30")
+
+
+
+
+
+
 
 
